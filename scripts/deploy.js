94d92818c2888a05e90e7e36929bf90156a470c0
Tom Montfort <tjmontfort12@gmail.com>
2022-12-18 17:56:25 -0500

    combined deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,7 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const ethers = require("ethers");
 
 async function main() {
   const MontyToken = await hre.ethers.getContractFactory("MontyToken");
@@ -13,6 +14,15 @@ async function main() {
   await montyToken.deployed();
 
   console.log("MontyToken deployed to:", montyToken.address);
+
+  const Vendor = await hre.ethers.getContractFactory("Vendor");
+  const vendor = await Vendor.deploy(montyToken.address);
+
+  await vendor.deployed();
+  console.log("Vendor deployed to:", vendor.address);
+
+  await montyToken.transfer(vendor.address, ethers.utils.parseEther("1000"));
+  console.log("Successfully transfered tokens to Vendor.")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
